195ab06eada2586b6c722772fc663ed410d88225
Muhanad2189 <136922152+Muhanad2189@users.noreply.github.com>
2025-04-17 15:05:03 +0200

    Added opening files and moving with cursor

--- main.py
+++ main.py
@@ -1,4 +1,61 @@
 import curses
 import os
 import sys
+import argparse
 import time
+
+class Window:
+    def __init__(self, n_rows, n_cols):
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+
+class Cursor:
+    def __init__(self, row=0, col=0):
+        self.row = row
+        self.col = col
+        
+    def up(self, buffer):
+        if self.row > 0:
+            self.row -= 1
+            self._clamp_col(buffer)
+
+    def down(self, buffer):
+        if self.row < len(buffer) - 1:
+            self.row += 1
+            self._clamp_col(buffer)
+
+    def _clamp_col(self, buffer):
+        self.col = min(self.col, len(buffer[self.row]))
+
+
+def main(stdscr):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    with open(args.filename) as f:
+        buffer = f.readlines()
+
+    window = Window(curses.LINES - 1, curses.COLS - 1)
+    cursor = Cursor()
+
+    while True:
+        stdscr.erase()
+        for row, line in enumerate(buffer[:window.n_rows]):
+            stdscr.addstr(row, 0, line[:window.n_cols])
+        stdscr.move(cursor.row, cursor.col)
+
+        k = stdscr.getkey()
+        if k == "q":
+            sys.exit(0)
+        elif k == "KEY_UP":
+            cursor.up(buffer)
+        elif k == "KEY_DOWN":
+            cursor.down(buffer)
+        elif k == "KEY_LEFT":
+            cursor.left()
+        elif k == "KEY_RIGHT":
+            cursor.right(buffer)
+
+if __name__ == "__main__":
+    curses.wrapper(main)
